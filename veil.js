3f1de9a7fbbacf3c7d1dbe7990954d4c5b609135
Gadzhiev Islam <gadzhievislam@mac.local>
2025-01-24 02:15:20 +0300

    [FIX] Decompose inner Veil logic to separate mwthods with checks

--- veil.js
+++ veil.js
@@ -5,45 +5,67 @@
  * not listed in the provided preset object
  */
 class Veil {
+  #pierced;
+  #target;
+  #presets;
+  #targetProto;
+
   constructor(
     target,
     presets,
-    pierced,
   ) {
-    this.pierced = false;
-    this.target = target;
-    this.presets = new Map(Object.entries(presets));
-
-    Object.getOwnPropertyNames(
-      Object.getPrototypeOf(this.target)
-    ).slice(1).forEach(
+    this.#pierced = false;
+    this.#target = target;
+    this.#presets = new Map(Object.entries(presets));
+    /**
+     * Accept the target object methods only (no)
+     */
+    this.#targetProto = Object.getPrototypeOf(this.#target);
+    Object.getOwnPropertyNames(this.#targetProto).forEach(
       methodName => {
-        if (typeof this.target[methodName] === 'function') {
-          this[methodName] = this.wrapMethod(methodName);
+        if (typeof this.#target[methodName] === 'function' && methodName !== 'constructor') {
+          this[methodName] = this.#useTargetMethod(methodName);
         }
       }
     );
+    
+  };
+  
+  #useTargetMethod(methodName) {
+    this.#ignoreSettersAndGetter(methodName);
+    return (...args) => {
+      return this.#acceptPresetMethodOrPierce(methodName, args);;
+    };
+  };
+  
+  /**
+   * Forbid setters and getters in the target object
+   */
+  #ignoreSettersAndGetter(methodName) {
+    var methodDescriptor = Object.getOwnPropertyDescriptor(this.#targetProto, methodName);
+    if (methodDescriptor.get || methodDescriptor.set)
+      throw new Error(`Method "${methodName}" is either setter or getter, which is forbidden.`);
+    return;
+  };
+  
+  #acceptPresetMethodOrPierce(methodName, ...args) {
+    if (!this.#pierced && this.#presets.has(methodName)) {
+      return this.#presets.get(methodName);
+    }
+    if (!this.#pierced) {
+      this.#pierce();
+    }
+    return this.#applyTargetMethod(methodName, args)
+  };
+  
+  #applyTargetMethod(methodName, ...args) {
+    var targetMethod = this.#target[methodName];
+    return targetMethod.apply(this.#target, args);
   };
 
-  wrapMethod(methodName) {
-    return (...args) => {
-      if (!this.pierced && this.presets.has(methodName)) {
-        return this.presets.get(methodName);
-      }
-      if (!this.pierced) {
-        this.pierce();
-      }
-      const targetMethod = this.target[methodName];
-      if (typeof targetMethod === 'function') {
-        return targetMethod.apply(this.target, args);
-      }
-      throw new Error(`Method "${String(methodName)}" does not exist on the target object.`);
-    };
-  }
-
-  pierce() {
-    this.pierced = true;
-  }
+  #pierce() {
+    this.#pierced = true;
+  };
 };
 
 export default Veil;
